0d8ee3bae174316b2a27a6c1b2ca7e53bbe47b32
Glen <glen84_dr@hotmail.com>
2022-10-11 00:01:11 +0400

    result of data in hash

--- part_1/lib/releve_compte_1.py
+++ part_1/lib/releve_compte_1.py
@@ -3,36 +3,36 @@
 #from operations import Operations
 
 class AncienSolde:
-    def __init__(self, data):
+    def __init__(self, data, dict_releve_compte = {}):
         self.data = data
-        self.dict_releve_compte = dict()
+        self.dict_releve_compte = dict_releve_compte
 
-        def ancien_solde_line():
-            self.dict_releve_compte["Code Engistrement"] = self.data[0:2]
-            self.dict_releve_compte["Code Banque"] = self.data[3:7]
-            self.dict_releve_compte["Account Nb"] = self.data[22:32]
-            self.dict_releve_compte["Ancien solde date"] = self.data[35:40]
-            if self.data[104] == "A":# | self.sold_line[104] == "B" | self.sold_line[104] == "C" | self.sold_line[104] == "D":
-                self.dict_releve_compte["Credit Montant"] = self.data[91:103]
-            else:
-                self.dict_releve_compte["Montant Credit"] = self.data[91:103]
-            return self.dict_releve_compte
+    def ancien_solde_line(self):
+        self.dict_releve_compte["Code Engistrement"] = self.data[0:2]
+        self.dict_releve_compte["Code Banque"] = self.data[3:7]
+        self.dict_releve_compte["Account Nb"] = self.data[22:32]
+        self.dict_releve_compte["Ancien solde date"] = self.data[35:40]
+        if self.data[104] == "A":# | self.sold_line[104] == "B" | self.sold_line[104] == "C" | self.sold_line[104] == "D":
+            self.dict_releve_compte["Credit Montant"] = self.data[91:103]
+        else:
+            self.dict_releve_compte["Montant Credit"] = self.data[91:103]
+        return self.dict_releve_compte
 
 class NewSolde:
     def __init__(self, data):
         self.data = data
         self.dict_releve_compte = {}
 
-        def new_solde_line():
-            self.dict_releve_compte["Code Engistrement"] = self.data[0:2]
-            self.dict_releve_compte["Code Banque"] = self.data[3:7]
-            self.dict_releve_compte["Account Nb"] = self.data[22:32]
-            self.dict_releve_compte["New solde date"] = self.data[35:40]
-            if self.data[104] == "A":# | self.sold_line[104] == "B" | self.sold_line[104] == "C" | self.sold_line[104] == "D":
-                self.dict_releve_compte["Credit Montant"] = self.data[91:103]
-            else:
-                self.dict_releve_compte["Montant Credit"] = self.data[91:103]
-            return self.dict_releve_compte
+    def new_solde_line(self):
+        self.dict_releve_compte["Code Engistrement"] = self.data[0:2]
+        self.dict_releve_compte["Code Banque"] = self.data[3:7]
+        self.dict_releve_compte["Account Nb"] = self.data[22:32]
+        self.dict_releve_compte["New solde date"] = self.data[35:40]
+        if self.data[104] == "A":# | self.sold_line[104] == "B" | self.sold_line[104] == "C" | self.sold_line[104] == "D":
+            self.dict_releve_compte["Credit Montant"] = self.data[91:103]
+        else:
+            self.dict_releve_compte["Montant Credit"] = self.data[91:103]
+        return self.dict_releve_compte
 
 
 class Operations:
@@ -40,16 +40,16 @@ class Operations:
         self.data = data
         self.dict_releve_compte = {}
 
-        def operation_line():
-            self.dict_releve_compte["Code Engistrement"] = self.data[0:2]
-            self.dict_releve_compte["Code Banque"] = self.data[3:7]
-            self.dict_releve_compte["Account Nb"] = self.data[22:32]
-            self.dict_releve_compte["Libelle of operation"] = self.data[49:79]
-            if self.data[104] == "A":# | self.sold_line[104] == "B" | self.sold_line[104] == "C" | self.sold_line[104] == "D":
-                self.dict_releve_compte["Credit Montant"] = self.data[91:103]
-            else:
-                self.dict_releve_compte["Montant Credit"] = self.data[91:103]
-            return self.dict_releve_compte
+    def operation_line(self):
+        self.dict_releve_compte["Code Engistrement"] = self.data[0:2]
+        self.dict_releve_compte["Code Banque"] = self.data[3:7]
+        self.dict_releve_compte["Account Nb"] = self.data[22:32]
+        self.dict_releve_compte["Libelle of operation"] = self.data[49:79]
+        if self.data[104] == "A":# | self.sold_line[104] == "B" | self.sold_line[104] == "C" | self.sold_line[104] == "D":
+            self.dict_releve_compte["Credit Montant"] = self.data[91:103]
+        else:
+            self.dict_releve_compte["Montant Credit"] = self.data[91:103]
+        return self.dict_releve_compte
 
 
 class ReleveBancaire:
@@ -60,9 +60,10 @@ class ReleveBancaire:
     def red_releve(self):
         for line in self.data:
             if line[0:2] == "01":
-                self.big_arr.append(AncienSolde(line).ancien_solde_line())
+                self.big_arr.append([AncienSolde(line).ancien_solde_line()])
             elif line[0:2] == "04":
-                self.big_arr.append(Operations(line).operation_line())
+                self.big_arr.append([Operations(line).operation_line()])
             elif line[0:2] == "07":
-                self.big_arr.append(NewSolde(line).dict_releve_compte())
-        self.big_arr
+                self.big_arr.append([NewSolde(line).new_solde_line()])
+        #print(self.big_arr)
+        return self.big_arr
--- part_1/main.py
+++ part_1/main.py
@@ -4,7 +4,7 @@ from lib.portionate_doc import AllLines
 from lib.releve_compte_1 import ReleveBancaire
 
 result = AllLines(doc_etebac).read_lines()
-test = ReleveBancaire(result).red_releve()
-print(test)
-#print(result)
+
+test2 = ReleveBancaire(result).red_releve()
+print(test2[0:3])
 #print(ReleveBancaire(result,AncienOuNouveauSolde()).read_selected())
