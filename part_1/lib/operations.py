f01abb6ba33d92ae8dae5b44b29b78cbe5ae9023
Glen <glen84_dr@hotmail.com>
2022-10-12 22:22:30 +0400

    correction part 1

--- part_1/lib/operations.py
+++ part_1/lib/operations.py
@@ -10,9 +10,5 @@ class Operations:
         self.dict_releve_compte["Account Nb"] = self.data[21:32]
         self.dict_releve_compte["Operation date"] = self.data[34:40]
         self.dict_releve_compte["operation name"] = self.data[49:79]
-        if self.data[103] == "A" or self.data[103] == "B" or self.data[103] == "C" or self.data[103] == "D" or self.data[103] == "E"\
-        or self.data[103] == "F" or self.data[103] == "G" or self.data[103] == "H" or self.data[103] == "H" or self.data[103] == "{":
-            self.dict_releve_compte["Credit Montant"] = self.data[91:103]
-        else:
-            self.dict_releve_compte["Debit Montant"] = self.data[91:103]
+        self.dict_releve_compte["Montant"] = self.data[91:103]
         return self.dict_releve_compte
